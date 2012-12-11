13da8a88a2ab3b0d337a6ae03c12672bcc155f99
Shivm lekhi <lekhishivam@gmail.com>
2012-12-11 20:35:11 +0530

    Frustrated

---
+++ key-bindings.el
@@ -0,0 +1,22 @@
+;;Custom Key Bindings
+
+;;eval and replace for emacs lisp
+(global-set-key [C-e C-r] 'fc-eval-and-replace)
+
+;;print basic html
+(global-set-key [C-tab] 'basic-html)
+
+;;Navigation between frames
+(global-set-key [M-left] 'windmove-left)    
+(global-set-key [M-right] 'windmove-right)  
+(global-set-key [M-up] 'windmove-up)        
+(global-set-key [M-down] 'windmove-down)    
+
+;;Multiple Cursors
+(global-set-key (kbd "C-S-d") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+
+(provide 'key-bindings)
